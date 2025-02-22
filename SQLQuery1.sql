8e0dd45b2dc66da293280cfc4b891462e5f7b259
DangTungDT <thanhtungdangdtt@gmail.com>
2025-02-22 15:01:43 +0700

    Update database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,5 +8,17 @@ create table NHANVIEN
 	id int not null,
 	Ho nvarchar(50),
 	Ten nvarchar(50),
+	idPhongBan int,
 	primary key(id)
 )
+go
+create table PHONGBAN
+(
+	id int not null,
+	Ten nvarchar(255),
+	primary key(id)
+)
+go
+
+alter table NHANVIEN
+add constraint fk_PhongBan_NhanVien foreign Key (id) references PHONGBAN(idPhongBan);
